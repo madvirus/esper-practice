91bee5f85a37bec80fd331c6155b5c6d2dc4b5c9
madvirus <madvirus@madvirus.net>
2014-02-03 18:26:06 +0900

    주식 샘플 얻기 위한 파서

--- esper-practice/src/test/java/stock/EsperTest.java
+++ esper-practice/src/test/java/stock/EsperTest.java
@@ -3,7 +3,12 @@ package stock;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,45 +29,59 @@ public class EsperTest {
 		config.addVariable("EVENT_STATE", Integer.class, 2);
 		epService = EPServiceProviderManager.getProvider("EsperTest", config);
 
-		EPStatement eps = epService.getEPAdministrator().createEPL(
-				"select * from ProcessEvent(state=EVENT_STATE)");
-
-		CompletedOrderSubscriber exampleSelect = new CompletedOrderSubscriber();
-		eps.setSubscriber(exampleSelect);
+//		EPStatement eps = epService.getEPAdministrator().createEPL(
+//				"select * from ProcessEvent(state=EVENT_STATE)");
+//
+//		CompletedOrderSubscriber exampleSelect = new CompletedOrderSubscriber();
+//		eps.setSubscriber(exampleSelect);
 
 		EPStatement eps2 = epService.getEPAdministrator().createEPL(
 				"select avg(totalPrice) " +
 						"from ProcessEvent(state=2).win:time(3 sec) " +
-						"output snapshot every 3 seconds");
+						"output every 4 seconds");
 		eps2.setSubscriber(new Object() {
+			@SuppressWarnings("unused")
 			public void update(Double avgPrice) {
-				System.out.println(new Date() + "Average orders for last 3 seconds: " + avgPrice);
+				System.out.println(now() + ": Average orders for last 3 seconds: " + avgPrice);
 			}
 		});
 	}
 
 	@Test
 	public void test() {
-		sleep(2000);
-		epService.getEPRuntime().sendEvent(new ProcessEvent("PurchaseOrder", 1L, 1, 5000, 1));
-		epService.getEPRuntime().sendEvent(new ProcessEvent("PurchaseOrder", 1L, 2, 10000, 1));
-		epService.getEPRuntime().sendEvent(new ProcessEvent("PurchaseOrder", 1L, 3, 15000, 1));
+		final List<ProcessEvent> events = new ArrayList<>();
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 1000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 2000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 3000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 4000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 5000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 6000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 7000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 8000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 9000, 2));
+		events.add(new ProcessEvent("PurchaseOrder", 1L, 1, 10000, 2));
 
-		epService.getEPRuntime().sendEvent(new ProcessEvent("PurchaseOrder", 1L, 1, 5000, 2));
-		epService.getEPRuntime().sendEvent(new ProcessEvent("PurchaseOrder", 1L, 2, 10000, 2));
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			int i = 0;
 
-		// epService.getEPRuntime().setVariableValue("EVENT_STATE", 1);
-		sleep(5000);
+			@Override
+			public void run() {
+				if (i < 10) {
+					System.out.printf(now() + " *** New Event Arrived, prcName=%s, price=%d\n",
+							events.get(i).getProcessName(), events.get(i).getTotalPrice());
+					epService.getEPRuntime().sendEvent(events.get(i));
+				}
+				i++;
+			}
+		};
+		timer.scheduleAtFixedRate(task, 300, 1000);
 
-		epService.getEPRuntime().sendEvent(new ProcessEvent("PurchaseOrder", 1L, 3, 15000, 2));
+		sleep(12000);
 
-		sleep(8000);
-		try {
-			Thread.sleep(8000);
-		} catch (InterruptedException e) {
-		}
+		timer.cancel();
 
-		assertThat(epService.getEPRuntime().getNumEventsEvaluated(), equalTo(6L));
+		assertThat(epService.getEPRuntime().getNumEventsEvaluated(), equalTo((long) events.size()));
 	}
 
 	private void sleep(long i) {
@@ -112,8 +131,13 @@ public class EsperTest {
 
 	public static class CompletedOrderSubscriber {
 		public void update(ProcessEvent event) {
-			System.out.printf(new Date() + "*** New Event Arrived, prcName=%s, procInstId=%d\n",
-					event.getProcessName(), event.getProcessInstanceId());
+			System.out.printf(now() + " *** New Event Arrived, prcName=%s, price=%d\n",
+					event.getProcessName(), event.getTotalPrice());
 		}
+	}
+
+	private static String now() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return df.format(new Date());
 	}
 }
---
+++ esper-practice/src/test/java/stock/StockDataLoaderTest.java
@@ -0,0 +1,146 @@
+package stock;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonParser.Feature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class StockDataLoaderTest {
+
+	public class Stock {
+		private String name;
+		private String code;
+		private int cost;
+		private int fluctuation;
+		private double rate;
+
+		public Stock(String name, String code, int cost, int fluctuation, double rate) {
+			this.name = name;
+			this.code = code;
+			this.cost = cost;
+			this.fluctuation = fluctuation;
+			this.rate = rate;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getCode() {
+			return code;
+		}
+
+		public int getCost() {
+			return cost;
+		}
+
+		public int getFluctuation() {
+			return fluctuation;
+		}
+
+		public double getRate() {
+			return rate;
+		}
+
+	}
+
+	@Test
+	public void read() {
+		List<Stock> stockList = loadKospi();
+		System.out.println(stockList.size());
+	}
+
+	private List<Stock> loadKospi() {
+		String jsonData = loadKospiStockJsonDataFromDaumStock();
+		List<Stock> stocks = new ArrayList<>(256);
+		ObjectMapper m = new ObjectMapper();
+		m.configure(Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+		try {
+			JsonNode rootNode = m.readTree(jsonData);
+			JsonNode listNode = rootNode.get("list");
+			Iterator<JsonNode> listItems = listNode.elements();
+			while (listItems.hasNext()) {
+				JsonNode listItem = listItems.next();
+				JsonNode itemNode = listItem.get("item");
+				Iterator<JsonNode> items = itemNode.elements();
+				while (items.hasNext()) {
+					JsonNode item = items.next();
+					stocks.add(createStock(item));
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return stocks;
+	}
+
+	private Stock createStock(JsonNode item) {
+		try {
+			String code = getNodeValue(item, "code");
+			String name = getNodeValue(item, "name");
+			DecimalFormat decimalFormat = new DecimalFormat("#,###,###,###");
+			int cost = decimalFormat.parse(getNodeValue(item, "cost")).intValue();
+			String updn = getNodeValue(item, "updn");
+			int fluctuation = 0;
+			if (updn.startsWith("▲")) {
+				fluctuation = decimalFormat.parse(updn.substring(1)).intValue();
+			} else if (updn.startsWith("▼")) {
+				fluctuation = -decimalFormat.parse(updn.substring(1)).intValue();
+			}
+			String rate = getNodeValue(item, "rate");
+			double ratevalue = 0.0;
+			if (!rate.equals("0.00%")) {
+				DecimalFormat decimalFormat2 = new DecimalFormat("+#,##0.00%;-#,##0.00%");
+				ratevalue = decimalFormat2.parse(rate).doubleValue();
+			}
+			return new Stock(code, name, cost, fluctuation, ratevalue);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private String getNodeValue(JsonNode item, String nodeName) {
+		String value = item.get(nodeName).toString();
+		return value.substring(1, value.length() - 1);
+	}
+
+	private String loadKospiStockJsonDataFromDaumStock() {
+		String jsonData = null;
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpGet getRequest = new HttpGet(
+				"http://stock.daum.net/xml/xmlallpanel.daum?stype=P&type=U");
+		try {
+			HttpResponse response = httpClient.execute(getRequest);
+			HttpEntity entity = response.getEntity();
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader((response.getEntity().getContent())));
+			StringBuilder builder = new StringBuilder();
+			String output;
+			while ((output = br.readLine()) != null) {
+				if (output.trim().equals("var dataset ="))
+					continue;
+				if (output.trim().equals(";"))
+					continue;
+				builder.append(output).append("\n");
+			}
+			jsonData = builder.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return jsonData;
+	}
+}
